6d8272abbb7ae30434232c12bfd21b3cc5b978fb
ernazar Nurbek uulu <ernazarnurbekuulu15@gmail.com>
2021-12-10 15:39:12 +0600

    first commit

---
+++ src/helpers/internationalize/internationalize.js
@@ -0,0 +1,40 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import translation_in_ky from "./translation_in_kg.json"
+import translation_in_ru from "./translation_in_ru.json"
+
+
+const resources = {
+	ky: {
+		translation: translation_in_ky,
+	},
+	ru: {
+		translation: translation_in_ru,
+	},
+
+};
+
+
+let selectedLanguague = localStorage.getItem("lang")
+	? localStorage.getItem("lang")
+	: "ru";
+
+
+i18n
+	.use(initReactI18next) // passes i18n down to react-i18next
+	.init({
+		resources,
+		lng: selectedLanguague,
+
+		keySeparator: false, // we do not use keys in form messages.welcome
+
+		interpolation: {
+			escapeValue: false, // react already safes from xss
+		},
+	});
+
+localStorage.setItem("lang", selectedLanguague);
+
+export default i18n;
+
+
---
+++ src/pages/ProjectsMap/ProjectsInMap.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from "styled-components"
+
+
+const ProjectsMap = () => {
+	return (
+		<Wrapper>
+			<div className="container">
+
+			</div>
+		</Wrapper>
+	)
+}
+
+export default ProjectsMap
+
+const Wrapper = styled.div`
+
+`
